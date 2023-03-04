29fa7a7118c14abba4760ea820f03b8b2dd37cce
Sofiane <ks_bessaha@esi.dz>
2023-03-04 18:46:17 +0100

    my app

--- Backend/routing.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import consumers
-
-# URLs that handle the WebSocket connection are placed here.
-websocket_urlpatterns=[
-    path(
-        'ws/chat/<str:chat_box_name>', consumers.ChatRoomConsumer.as_asgi()
-    ),
-]
